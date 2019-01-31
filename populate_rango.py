1dfdedc1ebfadc255f683fd7fa567bd4b02a8260
Jamie Moore <2324008m@student.gla.ac.uk>
2019-01-31 13:52:10 +0000

    5.8 pre-delete

--- populate_rango.py
+++ populate_rango.py
@@ -29,12 +29,12 @@ def populate():
         {"title": "Flask",
          "url": "http://flask.pocoo.org"} ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworkds": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 0, "likes": 0},
+            "Other Frameworkds": {"pages": other_pages, "views": 0, "likes": 0} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat.get("views"), cat.get("likes"))
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -49,8 +49,8 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     c.save()
     return c
 
--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
+class PageAdmin(admin.ModelAdmin):
+    def __init__(self):
+        super().__init__()
+
+    list_display = ('title', 'category', 'url')
+    
+
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
