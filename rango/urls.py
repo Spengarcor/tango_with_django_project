d176e37ad5feafbb7533783b9738d7afaf6dd16d
Spengarcor <2324008M@student.gla.ac.uk>
2019-02-07 20:48:49 +0000

    Please Work

--- rango/urls.py
+++ rango/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^about/', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^add_page/(?P<category_name_slug>[\w\-]+)$', views.add_page, name='add_page'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)$',
-        views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
 ]
