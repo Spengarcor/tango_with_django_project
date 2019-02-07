e20c01db6dd6c04fb00d3c9c50371a5d2c4ced03
Spengarcor <2324008M@student.gla.ac.uk>
2019-02-07 02:41:51 +0000

    Bugs fixed

--- populate_rango.py
+++ populate_rango.py
@@ -38,7 +38,7 @@ def populate():
          "views": 15} ]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
-            "Django": {"pages": django_pages, "views": 32, "likes": 16},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
             "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16} }
 
     for cat, cat_data in cats.items():
